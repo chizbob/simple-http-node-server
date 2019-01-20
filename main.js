28e3f2c7b05beff55e30e9ca97abf09a89bb6bb3
Soo Kim <chizbob@gmail.com>
2019-01-19 21:56:31 -0800

    resolved

--- main.js
+++ main.js
@@ -2,17 +2,13 @@ var http = require('http')
 var fs = require('fs')
 var server = http.createServer(function(request, response){
 //  console.log(`request was made: ${request.url}`)
-  var url = request.url
-
-  try {
-    fs.appendFileSync('hello-world.txt', 'Hello to this great world');
-    console.log('appended to file!');
-  } catch (err) {
-    console.log("error")
-  }
+  fs.appendFile('hello-world.txt', 'Hello to this great world', function (err){
+  if (err) throw err
+  console.log('Saved!')
+  })
 
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end(fs.readFileSync(__dirname + url))
+  response.end('howdy')
 })
 
 server.listen(3000)
